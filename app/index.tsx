b58d14471e80061820f0d5ef970db951f16c305f
Maxime-Lointier <157472240+Maxime-Lointier@users.noreply.github.com>
2025-11-13 09:55:45 +0100

    fix tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,9 @@
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function HomeScreen() {
-  return <View className="flex-1 bg-white" />;
+  return (
+    <View className="flex-1 items-center justify-center bg-red-500">
+      <Text className="text-4xl text-white">Test NativeWind</Text>
+    </View>
+  );
 }
