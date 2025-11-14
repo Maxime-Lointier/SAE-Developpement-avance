ac5ed9ae6ab4dd41bd90793fd190d5b7070b1658
DenisVuong <164088452+DenisVuong@users.noreply.github.com>
2025-11-14 15:29:49 +0100

    genre + css v1

--- app/onBoarding.tsx
+++ app/onBoarding.tsx
@@ -1,6 +1,6 @@
 import { Link } from "expo-router";
 import { useEffect, useState } from "react";
-import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
+import { Image, Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import "./global.css"
 
 // --- Configuration de l'API ---
@@ -39,10 +39,62 @@ const onBoarding = () => {
     }
     
     return (
-        <ScrollView>
-            {genre.map((genre) => (
-                <Button title={genre.name}/>
-            ))}
+        <ScrollView style={{backgroundColor:"#0f0f1e", flex: 1}}>
+            <View style={{ maxWidth: 400, alignSelf: 'center', width: '100%', paddingHorizontal: 16 }}>
+
+                //Text : Title
+                <Text style={{color: "white", fontWeight: "bold", fontSize: 30, textAlign: "center", marginBottom: 8}}>
+                    Vos genres préférés
+                </Text>
+
+                //Text : Description
+                <Text style={{color: "#99a0ac", fontSize: 16, textAlign: "center", marginBottom: 24}}>
+                    Selectionnez au moins 3 genres pour personnaliser vos recommandations
+                </Text>
+
+                //Button : Genre
+                <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+                    {genre.map((genre) => {
+                        return (
+                            <Pressable 
+                                key={genre.id} 
+                                style={{
+                                    backgroundColor: "#1f1f37",
+                                    padding: 16,
+                                    margin: 4,
+                                    borderRadius: 16,
+                                    width: '48%',
+                                    minHeight: 100,
+                                    justifyContent: 'center',
+                                    alignItems: 'center'
+                                }}
+                            >
+                                //Logo : Movie
+                                <Image 
+                                    source={{ uri: "https://img.icons8.com/sf-black/64/movie.png" }}
+                                    style={{ width: 32, height: 32, marginBottom: 8, tintColor: 'white' }}
+                                />
+
+                                //Text: Genre Name
+                                <Text style={{ color: 'white', textAlign: 'center', fontWeight: '600', fontSize: 16 }}>
+                                    {genre.name}
+                                </Text>
+
+                            </Pressable>
+                        );
+                    })}
+                </View>
+
+                //Text : Number of genres selected
+                <Text style={{color: "white", textAlign: "center", marginTop: 16, marginBottom: 8}}>
+                    0/3 genres selectionnés
+                </Text>
+
+                //Button : Continue
+                <Pressable>
+                    <Text style={{color: "white", textAlign: "center"}}>Continuer</Text>
+                </Pressable>
+            </View>
         </ScrollView> 
     )
 }
